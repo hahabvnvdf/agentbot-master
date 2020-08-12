cb68b73be6590a038781eca8dbcd5703b3db3a04
Duy Pham Le <phamleduy04@gmail.com>
2020-08-11 23:28:18 -0500

    fix bugs

--- commands/fun/crush.js
+++ commands/fun/crush.js
@@ -4,7 +4,7 @@ module.exports = {
     description: "Tìm crush của bạn",
     cooldown: 2,
     run: async(client, message, args) => {
-        let person = message.guild.members.cache.filter(m => !m.user.bot && m.user.id == message.author.id).random()
+        let person = message.guild.members.cache.filter(m => !m.user.bot && m.user.id !== message.author.id).random()
         if (!person) person = message.author;
         message.channel.send(`**${person.displayName}** muốn xơi **${message.member.displayName}** từ lâu......`);
     }
