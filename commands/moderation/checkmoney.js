1404fc253c543ea8e59c9bfc11b50a3006e2d1a2
Duy Pham Le <phamleduy04@gmail.com>
2020-08-22 21:18:02 -0500

    fix bugs

--- commands/moderation/checkmoney.js
+++ commands/moderation/checkmoney.js
@@ -1,9 +1,11 @@
 const Eco = require('quick.eco');
 const eco = new Eco.Manager();
+const { ownerID } = require('../../config.json');
 module.exports = {
     name: 'checkmoney',
     description: 'Kiểm tra tiền (owner bot only)',
     run: async (client, message, args) => {
+        if (message.author.id !== ownerID) return message.channel.send('Chỉ có owner của bot mới có thể sử dụng lệnh này');
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
         let data = await eco.fetchMoney(member.id);
         message.channel.send(JSON.stringify(data, null, 4), { code: "json" });
