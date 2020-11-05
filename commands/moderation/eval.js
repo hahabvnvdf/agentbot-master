895e38ca110df7998d3168e356cd1d8e2c08df5b
Duy Pham Le <phamleduy04@gmail.com>
2020-11-05 11:48:40 -0600

    set xp = 0 when leveled up

--- commands/moderation/eval.js
+++ commands/moderation/eval.js
@@ -7,18 +7,17 @@ module.exports = {
         description: "Execute javascript code",
         usage: "<PREFIX>eval <js code>",
         run: async (client, message, args) => {
-            if (message.author.id == ownerID) {
-                if (!args[0]) return message.reply('Nhập lệnh để chạy code...');
-                try {
-                    const start = process.hrtime();
-                    let output = eval(args.join(' '));
-                    const difference = process.hrtime(start);
-                    if (typeof output !== "string") output = inspect(output, { depth: 2 });
-                    return message.channel.send(`*Lệnh đã chạy xong trong ${difference[0] > 0 ? `${difference[0]}s ` : ""}${difference[1] / 1e6}ms*\n\`\`\`js\n${output.length > 1950 ? await post(output) : output}\n\`\`\``);
-                }
-                catch(err) {
-                    return message.channel.send(`Error:\n\`${err}\``);
-                }
-            } else return message.channel.send("Lệnh này chỉ dành cho owner của bot.");
+            if (message.author.id != ownerID) return message.channel.send("Lệnh này chỉ dành cho owner của bot.");
+            if (!args[0]) return message.reply('Nhập lệnh để chạy code...');
+            try {
+                const start = process.hrtime();
+                let output = eval(args.join(' '));
+                const difference = process.hrtime(start);
+                if (typeof output !== "string") output = inspect(output, { depth: 2 });
+                return message.channel.send(`*Lệnh đã chạy xong trong ${difference[0] > 0 ? `${difference[0]}s ` : ""}${difference[1] / 1e6}ms*\n\`\`\`js\n${output.length > 1950 ? await post(output) : output}\n\`\`\``);
+            }
+            catch(err) {
+                return message.channel.send(`Error:\n\`${err}\``);
+            }
     },
 };
--- index.js
+++ index.js
@@ -144,10 +144,11 @@ client.on("message", async message => {
         if (!userdata) userdata = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, xp: 0, level: 1 };
         if (userdata.level !== 999) {
         const xpAdd = Math.floor(Math.random() * 12);
-        const nextlvl = userdata.level * 300;
+        const nextlvl = userdata.level * 200;
         if(userdata.xp > nextlvl) {
             userdata.level++;
             message.reply(`Bạn đã lên cấp **${userdata.level}**!`);
+            userdata.xp = 0;
         }
         userdata.xp += xpAdd;
         client.setScore.run(userdata);
