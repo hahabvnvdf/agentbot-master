41f42cd660b4aaae19ef24c90aa8d77ebaf9961d
Duy Pham Le <phamleduy04@gmail.com>
2020-11-08 21:51:49 -0600

    perm fix

--- commands/settings/ailanguage.js
+++ commands/settings/ailanguage.js
@@ -6,6 +6,7 @@ module.exports = {
     description: 'Thay đổi ngôn ngữ AI',
     category: 'settings',
     run: async (client, message, args) => {
+        if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply('Bạn cần có quyền `MANAGE_GUILD` để chạy lệnh này!');
         if (!args[0] || !ansAccept.includes(args[0])) return message.channel.send('Vui lòng nhập `en` hoặc `vi` để cài đặt ngôn ngữ!');
         await db.set(`${message.guild.id}.aiLang`, args[0]);
     },
