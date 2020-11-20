a025bb3fa7b4b974cd8266471a5468d2f4880a68
Duy Pham Le <phamleduy04@gmail.com>
2020-11-20 14:54:05 -0600

    làm gọn

--- commands/info/corona.js
+++ commands/info/corona.js
@@ -13,16 +13,17 @@ module.exports = {
     run: async (client, message, args) => {
         if (!args[0]) {
             const data = await api.all();
-            const d = new Date(data.updated);
+            const { updated, cases, todayCases, deaths, todayDeaths, critical, recovered, affectedCountries } = data;
+            const d = new Date(updated);
             const fulldate = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
             const embed = new MessageEmbed()
                 .setAuthor('Dữ liệu tự động cập nhật')
                 .setTitle(`Số ca nhiễm COVID-19 ở Thế Giới`)
-                .addField('Số ca nhiễm: ', `${laysodep(data.cases)}(+${laysodep(data.todayCases)})`, true)
-                .addField('Số ca tử vong: ', `${laysodep(data.deaths)}(+${laysodep(data.todayDeaths)})`, true)
-                .addField('Số ca nghiêm trọng: ', laysodep(data.critical), true)
-                .addField('Số ca hồi phục: ', laysodep(data.recovered), true)
-                .addField('Số quốc gia bị nhiễm: ', data.affectedCountries, true)
+                .addField('Số ca nhiễm: ', `${laysodep(cases)}(+${laysodep(todayCases)})`, true)
+                .addField('Số ca tử vong: ', `${laysodep(deaths)}(+${laysodep(todayDeaths)})`, true)
+                .addField('Số ca nghiêm trọng: ', laysodep(critical), true)
+                .addField('Số ca hồi phục: ', laysodep(recovered), true)
+                .addField('Số quốc gia bị nhiễm: ', affectedCountries, true)
                 .addField('Ngày cập nhật: ', fulldate, true)
                 .setFooter('Nguồn: worldometers.info');
             message.channel.send(embed);
@@ -30,13 +31,14 @@ module.exports = {
             if (!args[1]) return message.channel.send('Vui lòng nhập tên bang!');
             const statename = args.splice(1).join(' ');
             const data = await api.states({ state: statename });
+            const { state, cases, todayCases, deaths, todayDeaths, tests, active } = data;
             if (data.message) return message.channel.send('Không tìm thấy bang mà bạn yêu cầu');
             const embed = new MessageEmbed()
-                .setAuthor(`Thông tin về COVID-19 ở bang ${data.state}`)
-                .addField('Tất cả ca nhiễm: ', `${laysodep(data.cases)}(+${laysodep(data.todayCases)})`, true)
-                .addField('Số ca tử vong: ', `${laysodep(data.deaths)}(+${laysodep(data.todayDeaths)})`, true)
-                .addField('Số ca đã thử virus: ', laysodep(data.tests), true)
-                .addField('Số ca nhiễm hiện tại: ', laysodep(data.active), true)
+                .setAuthor(`Thông tin về COVID-19 ở bang ${state}`)
+                .addField('Tất cả ca nhiễm: ', `${laysodep(cases)}(+${laysodep(todayCases)})`, true)
+                .addField('Số ca tử vong: ', `${laysodep(deaths)}(+${laysodep(todayDeaths)})`, true)
+                .addField('Số ca đã thử virus: ', laysodep(tests), true)
+                .addField('Số ca nhiễm hiện tại: ', laysodep(active), true)
                 .setFooter('Nguồn: worldometers.info');
             message.channel.send(embed);
         } else if (args[0] == 'usprovince') {
@@ -56,29 +58,31 @@ module.exports = {
             state = capitalizeWords(state.trim());
             let data = await api.jhucsse.counties({ county: province_name });
             data = data.filter(e => e.province == state);
-            data = data[0];
-            const d = new Date(data.updatedAt);
+            const { county, province, stats, updatedAt } = data[0];
+            const { confirmed, deaths, recovered } = stats;
+            const d = new Date(updatedAt);
             const date = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
             const embed = new MessageEmbed()
-                .setAuthor(`Thông tin về COVID-19 ở quận ${data.county},${data.province}`)
-                .addField('Số ca nhiễm: ', laysodep(data.stats.confirmed), true)
-                .addField('Số ca tử vong: ', laysodep(data.stats.deaths), true)
-                .addField('Số ca hồi phục: ', laysodep(data.stats.recovered), true)
+                .setAuthor(`Thông tin về COVID-19 ở quận ${county},${province}`)
+                .addField('Số ca nhiễm: ', laysodep(confirmed), true)
+                .addField('Số ca tử vong: ', laysodep(deaths), true)
+                .addField('Số ca hồi phục: ', laysodep(recovered), true)
                 .addField('Ngày cập nhật: ', date, true)
                 .setFooter('Source: John Hopkins University');
             message.channel.send(embed);
         } else {
             const data = await api.countries({ country: args.join(' ') });
+            const { updated, country, countryInfo, cases, todayCases, deaths, todayDeaths, critical, recovered } = data;
             if (data.message) return message.channel.send('Mình không tìm thấy tên nước mà bạn nhập.');
-            const d = new Date(data.updated);
+            const d = new Date(updated);
             const fulldate = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
             const embed = new MessageEmbed()
                 .setAuthor(`Thông tin về COVID-19 ở ${data.country}`)
-                .setThumbnail(data.countryInfo.flag)
-                .addField('Số ca nhiễm: ', `${laysodep(data.cases)}(+${laysodep(data.todayCases)})`, true)
-                .addField('Số ca tử vong: ', `${laysodep(data.deaths)}(+${laysodep(data.todayDeaths)})`, true)
-                .addField('Số ca nghiêm trọng: ', laysodep(data.critical), true)
-                .addField('Số ca hồi phục: ', laysodep(data.recovered), true)
+                .setThumbnail(countryInfo.flag)
+                .addField('Số ca nhiễm: ', `${laysodep(cases)}(+${laysodep(todayCases)})`, true)
+                .addField('Số ca tử vong: ', `${laysodep(deaths)}(+${laysodep(todayDeaths)})`, true)
+                .addField('Số ca nghiêm trọng: ', laysodep(critical), true)
+                .addField('Số ca hồi phục: ', laysodep(recovered), true)
                 .addField('Ngày cập nhật: ', fulldate, true)
                 .setFooter('Nguồn: worldometers.info');
             message.channel.send(embed);
