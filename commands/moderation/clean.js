fb4de87c181285d60b5e212d35e827118b06b371
Duy Pham Le <phamleduy04@gmail.com>
2020-12-25 16:14:23 -0600

    Update clean.js

--- commands/moderation/clean.js
+++ commands/moderation/clean.js
@@ -5,14 +5,12 @@ module.exports = {
     usage: '<PREFIX>clean',
     run: async (client, message, args) => {
         if (message.deletable) await message.delete();
-        if (!message.member.hasPermission("MANAGE_MESSAGES"))
-            return message.reply("Bạn không có quyền MANAGE_MESSAGES").then(m => m.delete({ timeout: 5000 }));
         message.channel.messages.fetch({
             limit: 100,
         }).then(async messages => {
             messages = messages.filter(msg => msg.author.id === client.user.id).array();
             await message.channel.bulkDelete(messages, true);
-            await message.channel.send('👍');
+            await message.channel.send('👍').then(m => m.delete({ timeout: 5000 }));
         });
     },
 };
