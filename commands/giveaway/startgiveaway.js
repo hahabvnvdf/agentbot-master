55754ab90ce797483d17f9c2ced21f4bb50413b8
Duy Pham Le <phamleduy04@gmail.com>
2021-02-07 00:04:15 -0600

    changes

--- commands/giveaway/startgiveaway.js
+++ commands/giveaway/startgiveaway.js
@@ -8,7 +8,8 @@ module.exports = {
     usage: '<PREFIX>start <thời gian GA(10s, 20m, 1d)> <số người thắng> <phần thưởng>',
     example: '<PREIFX>start 1d 1 100000 agent money',
     run: async (client, message, args, guildData) => {
-        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("Bạn không có quyền `MANAGE_CHANNELS`");
+        const userRoles = message.member.roles.cache.map(r => r.name.toLowerCase());
+        if (!message.member.hasPermission('MANAGE_CHANNELS') && !userRoles.includes('giveaway')) return message.reply("Bạn không có quyền `MANAGE_CHANNELS`");
         if (!args[0] || !args[1] || !args[2]) return message.channel.send(`Bạn hãy nhập đủ dữ liệu cho bot đi!\n\n**TIP:** Sử dụng lệnh \`${guildData.prefix}help start\` để biết cách sử dụng!`);
         if (message.deletable) message.delete();
         client.giveawaysManager.start(message.channel, {
@@ -16,8 +17,6 @@ module.exports = {
             winnerCount: parseInt(args[1]),
             prize: args.slice(2).join(' '),
             messages: ga.message,
-        }).then(gaData => {
-            console.log(gaData);
         });
     },
 };
